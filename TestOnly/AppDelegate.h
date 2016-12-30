1a1a44d8b9f5240f1cdb6552e416824196c1cfe4
文楷 <wanlei.gwl@alibaba-inc.com>
2016-12-30 16:50:05 +0800

    commit project

---
+++ TestOnly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOnly
+//
+//  Created by Wanlei on 2016/12/30.
+//  Copyright © 2016年 organizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOnly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOnly
+//
+//  Created by Wanlei on 2016/12/30.
+//  Copyright © 2016年 organizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
